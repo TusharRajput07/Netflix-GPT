ddee298124c67e85ced6570b64bb8108b0acb004
TusharRajput07 <tusharrajput2002@gmail.com>
2024-11-23 18:52:49 +0530

    added gemini search and restricted routing

--- src/utils/openai.js
+++
@@ -1,9 +0,0 @@
-import OpenAI from "openai";
-
-const client = new OpenAI({
-  apiKey:
-    "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", // This is the default and can be omitted
-  dangerouslyAllowBrowser: true,
-});
-
-export default client;
